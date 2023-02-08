7d80fc36d99b4c98889964846b14d5d3b613d537
mineejo <evgenyshaimov@gmail.com>
2023-02-08 19:33:22 +0300

    Updated to version 0.2.2.

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,5 @@
-import 'components/app.reio.dart';
+import 'index.widgets.dart';
 
 void main() {
-  // If this is your first time using Reio, it is highly
-  // it is highly recommended that you open the documentation of the framework.
-  // https://github.com/MineEjo/reiodart/wiki
-
-  /// Creating a component App.
-  $App();
+  widgetConstruction();
 }
